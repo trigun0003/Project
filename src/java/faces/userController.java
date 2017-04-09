59738aa60e29bce3fc1190db0ee3c24d11d15329
bellahuang <bellahuang@192.168.0.8>
2017-04-09 10:56:32 -0400

    userController finished

--- src/java/faces/userController.java
+++ src/java/faces/userController.java
@@ -5,10 +5,63 @@
  */
 package faces;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
 /**
  *
  * @author Heber
  */
+@ManagedBean
+@ApplicationScoped
 public class userController {
+    private static List<user> users = new ArrayList<>();
+    
+    public userController(){
+        getUserFromDB();
+    }
+    
+    public static void getUserFromDB(){
+        users = new ArrayList<>();
+        try {
+            Connection conn = DBUtils.getConnection();
+            String query = "SELECT * FROM users";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+            user u = new user(
+                    rs.getInt("user_id"), 
+                    rs.getString("user_name"), 
+                    rs.getString("email"), 
+                    rs.getString("password"), 
+                    rs.getDate("create_date"), 
+                    rs.getInt("user_type"),
+                    rs.getString("first_name"),
+                    rs.getString("last_name")
+            );
+            users.add(u);
+        }
+        } catch (SQLException ex) {
+            Logger.getLogger(userController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public List<user> getUsers() {
+        return users;
+    }
+    
+    public user getUserById(int id){
+        for (user u : users){
+            if (u.getUser_id() == id){
+                return u;
+            }
+        }
+        return null;
+    }
     
 }
