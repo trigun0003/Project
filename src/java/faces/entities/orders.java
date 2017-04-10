938711ddc5a99fadc2c8592687ac555ac7416cf6
trigun0003 <hebert54@hotmail.com>
2017-04-10 17:16:56 -0400

    items

---
+++ src/java/faces/entities/orders.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faces.entities;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ *
+ * @author c0687631
+ */
+public class orders {
+    private int order_number;
+    private int item_id;
+    private String order_date;
+    
+    public orders()
+    {
+        
+    }
+
+    public orders(int order_number, int item_id, String order_date) {
+        this.order_number = order_number;
+        this.item_id = item_id;
+        this.order_date = order_date;
+    }
+
+    public int getOrder_number() {
+        return order_number;
+    }
+
+    public void setOrder_number(int order_number) {
+        this.order_number = order_number;
+    }
+
+    public int getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(String order_date) {
+        this.order_date = order_date;
+    }
+    
+    public JsonObject toJson() {
+        return Json.createObjectBuilder()
+                .add("order_number", order_number)
+                .add("item_id", item_id)
+                .add("order_date", order_date)
+                .build();
+    }
+    
+}
