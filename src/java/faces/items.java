5ec49aaa4407cef93a2565e2068f2ce285173e3d
trigun0003 <hebert54@hotmail.com>
2017-04-12 10:58:01 -0400

    Merge origin/development into development
    Conflicts:
    	src/java/faces/orderController.java
    	src/java/faces/orders.java

--- src/java/faces/items.java
+++ src/java/faces/items.java
@@ -6,6 +6,7 @@
 package faces;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.logging.Level;
@@ -25,6 +26,11 @@ public class items {
    private int item_price;
    private int user_id;
    private File picture;
+   private int tag_id;
+
+    
+
+    
 
    
      /**
@@ -34,13 +40,13 @@ public class items {
 
     }
 
-    public items(int item_id, String item_name, String description, int item_price, int user_id) {
+    public items(int item_id, String item_name, String description, int item_price, int user_id, int tag_id) {
         this.item_id = item_id;
         this.item_name = item_name;
         this.description = description;
         this.item_price = item_price;
         this.user_id = user_id;
-        //this.picture = picture;
+        this.tag_id = tag_id;
     }
     
     
@@ -50,6 +56,7 @@ public class items {
         description = json.getString("item_description", "");
         item_price = json.getInt("item_price", 0);
         user_id = json.getInt("user_id", 0);
+        tag_id = json.getInt("tag_id", 0);
         //picture = (File)json.getString("PICTURE", "");
     }
 
@@ -105,6 +112,14 @@ public class items {
     public void setPicture(File picture) {
         this.picture = picture;
     }
+    
+    public int getTag_id() {
+        return tag_id;
+    }
+
+    public void setTag_id(int tag_id) {
+        this.tag_id = tag_id;
+    }
   
     public JsonObject toJson() {
         return Json.createObjectBuilder()
@@ -113,7 +128,7 @@ public class items {
                 .add("description", description)
                 .add("item_price", item_price)
                 .add("user_id", user_id)
-                //.add("picture", picture.toString())
+                .add("tag_id", tag_id)
                 .build();
     }
     
